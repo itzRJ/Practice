50ff3fcff575c3ec8e85389cc2a90b4304f78413
Rahul Jhamb <37768824+itzRJ@users.noreply.github.com>
2022-09-12 11:30:24 +0530

    Initial Commit

---
+++ Practice/src/main/java/LinkedList.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class LinkedList{
+    int val;
+    LinkedList next;
+
+    public LinkedList(){}
+    public LinkedList(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
