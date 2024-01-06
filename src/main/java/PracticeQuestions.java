ffb1c8a2dbe4a6833b5d01fa8bb8762c56553395
Rahul Jhamb <rahul5.mukesh@gmail.com>
2024-01-06 16:07:13 +0530

    Maximum Profit in Job Scheduling Question: https://leetcode.com/problems/maximum-profit-in-job-scheduling/ RJ

--- src/main/java/PracticeQuestions.java
+++ src/main/java/PracticeQuestions.java
@@ -1571,4 +1571,62 @@ public class PracticeQuestions {
         }
         return res;
     }
+
+    /**
+     * Using bimary search and dp to find non overlapping jobs and maximising profit
+     * @param startTime
+     * @param endTime
+     * @param profit
+     * @return Max profit
+     */
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int numJobs = profit.length; // Number of jobs
+        Job[] jobs = new Job[numJobs];
+
+        for (int i = 0; i < numJobs; ++i) {
+            jobs[i] = new Job(endTime[i], startTime[i], profit[i]);
+        }
+
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job.endTime));
+        int[] dp = new int[numJobs + 1];
+
+        for (int i = 0; i < numJobs; ++i) {
+            int startTimeValue = jobs[i].startTime;
+            int profitValue = jobs[i].profit;
+
+            int latestNonConflictJobIndex = upperBound(jobs, i, startTimeValue);
+            dp[i + 1] = Math.max(dp[i], dp[latestNonConflictJobIndex] + profitValue);
+        }
+
+        return dp[numJobs];
+    }
+
+    private int upperBound(Job[] jobs, int endIndex, int targetTime) {
+        int low = 0;
+        int high = endIndex;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (jobs[mid].endTime <= targetTime) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    private static class Job {
+        int endTime;
+        int startTime;
+        int profit;
+
+        public Job(int endTime, int startTime, int profit) {
+            this.endTime = endTime;
+            this.startTime = startTime;
+            this.profit = profit;
+        }
+
+    }
 }
