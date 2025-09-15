ec2f1d61c7f366595e8a8c287a6956f252580957
itzRJ <rahul5.mukesh@gmail.com>
2025-09-15 21:12:56 +0530

    Largest Number: https://leetcode.com/problems/largest-number/

--- src/main/java/PracticeQuestions.java
+++ src/main/java/PracticeQuestions.java
@@ -1629,4 +1629,22 @@ public class PracticeQuestions {
         }
 
     }
+
+    public static String largestNumber(int[] nums) {
+        StringBuilder res = new StringBuilder();
+        String[] numsString = new String[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            numsString[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(numsString, (a, b) -> (b+a).compareTo(a+b));
+
+        if(numsString[0].equals("0"))
+            return "0";
+
+        for(String s:numsString)
+            res.append(s);
+
+        return res.toString();
+    }
 }
